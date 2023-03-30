7bb89c55f6ad51e99f74f64aba69f93700651070
jwalcher <walcher@uni-heidelberg.de>
2023-03-30 18:32:08 +0200

    workaround working

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,47 @@
 // export const dynamic = “force-dynamic”; // enabling this: data is still cached indefinitely
-// export const fetchCache = "force-no-store";   // enabling this: data is still cached indefinately
+// export const fetchCache = "force-cache";   // enabling this: data is still cached indefinately
 // export const revalidate = 0; //  enabling this: data is revalidated after ~10 secs
 
-import { ApolloClient, InMemoryCache, HttpLink, from, ApolloLink, gql } from "@apollo/client";
-import { onError } from "@apollo/client/link/error";
-import moment from "moment";
+// import { ApolloClient, InMemoryCache, HttpLink, from, ApolloLink, gql } from "@apollo/client";
+// import { onError } from "@apollo/client/link/error";
+// import moment from "moment";
 
-const client = new ApolloClient({
-  uri: "http://localhost:1337/graphql",
-  cache: new InMemoryCache(),
-  ssrMode: typeof window === "undefined",
-  name: "react-web-client",
-  connectToDevTools: true,
-  // caching be done by Next
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: "no-cache",
-      errorPolicy: "ignore",
-    },
-    query: {
-      fetchPolicy: "no-cache",
-      errorPolicy: "all",
-    },
-  },
-});
+// const client = new ApolloClient({
+//   uri: "http://localhost:1337/graphql",
+//   cache: new InMemoryCache(),
+//   ssrMode: typeof window === "undefined",
+//   name: "react-web-client",
+//   connectToDevTools: true,
+//   // caching be done by Next
+//   defaultOptions: {
+//     watchQuery: {
+//       fetchPolicy: "no-cache",
+//       errorPolicy: "ignore",
+//     },
+//     query: {
+//       fetchPolicy: "no-cache",
+//       errorPolicy: "all",
+//     },
+//   },
+// });
 
-async function getData() {
-  const { data, error } = await client.query({
-    query: gql`
-      {
-        front {
-          data {
-            id
-            attributes {
-              updatedAt
-            }
-          }
-        }
-      }
-    `,
-  });
-  return data;
-}
+// async function getData() {
+//   const { data, error } = await client.query({
+//     query: gql`
+//       {
+//         front {
+//           data {
+//             id
+//             attributes {
+//               updatedAt
+//             }
+//           }
+//         }
+//       }
+//     `,
+//   });
+//   return data;
+// }
 
 export default async function Home() {
   const res = await fetch(
@@ -51,7 +51,7 @@ export default async function Home() {
 
   const data = (await res.json()) as { hour: number; minute: number; seconds: number };
 
-  const strapidata = await getData();
+  // const strapidata = await getData();
 
   return (
     <>
@@ -59,10 +59,10 @@ export default async function Home() {
         Time in Amsterdam is {data.hour}:{data.minute}:{data.seconds}
       </h1>
       {/* <p>{JSON.stringify(strapidata)}</p> */}
-      <h1>
-        Strapi updatedAt:&nbsp;
-        {moment(strapidata.front.data.attributes.updatedAt as string).format("HH:mm:ss")}
-      </h1>
+      {/* <h1> */}
+        {/* Strapi updatedAt:&nbsp; */}
+        {/* {moment(strapidata.front.data.attributes.updatedAt as string).format("HH:mm:ss")} */}
+      {/* </h1> */}
     </>
   );
 }
