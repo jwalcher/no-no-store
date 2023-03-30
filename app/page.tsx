a49b452419fdd74e97a567a655dfcba1fe334e29
jwalcher <walcher@uni-heidelberg.de>
2023-03-30 14:17:14 +0200

    workaround

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,68 @@
-
 // export const dynamic = “force-dynamic”; // enabling this: data is still cached indefinitely
 // export const fetchCache = "force-no-store";   // enabling this: data is still cached indefinately
-// export const revalidate = 10 //  enabling this: data is revalidated after ~10 secs
+// export const revalidate = 0; //  enabling this: data is revalidated after ~10 secs
+
+import { ApolloClient, InMemoryCache, HttpLink, from, ApolloLink, gql } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import moment from "moment";
+
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+  ssrMode: typeof window === "undefined",
+  name: "react-web-client",
+  connectToDevTools: true,
+  // caching be done by Next
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "ignore",
+    },
+    query: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "all",
+    },
+  },
+});
+
+async function getData() {
+  const { data, error } = await client.query({
+    query: gql`
+      {
+        front {
+          data {
+            id
+            attributes {
+              updatedAt
+            }
+          }
+        }
+      }
+    `,
+  });
+  return data;
+}
 
 export default async function Home() {
   const res = await fetch(
-    "https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam",
+    "https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Amsterdam"
     // { cache: "no-store" } // enabling this: data is not cached
   );
 
   const data = (await res.json()) as { hour: number; minute: number; seconds: number };
 
+  const strapidata = await getData();
+
   return (
-    <h1>
-      Time in Amsterdam is {data.hour}:{data.minute}:{data.seconds}
-    </h1>
+    <>
+      <h1>
+        Time in Amsterdam is {data.hour}:{data.minute}:{data.seconds}
+      </h1>
+      {/* <p>{JSON.stringify(strapidata)}</p> */}
+      <h1>
+        Strapi updatedAt:&nbsp;
+        {moment(strapidata.front.data.attributes.updatedAt as string).format("HH:mm:ss")}
+      </h1>
+    </>
   );
 }
--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,7 +1,8 @@
 // navigating to http://localhost:3000/api/revalidate
 // revalidates up to and including 13.1.6, but not in latest canary
 
-
+import fs from "fs";
+import path from "path";
 
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -10,13 +11,29 @@ type Data =
       message: string;
     }
   | { revalidated: Boolean }
+  | { cacheCleared: Boolean }
   | string;
+
+const directory = process.cwd() + "/.next/cache/fetch-cache";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   try {
-    console.log("revalidating");
-    await res.revalidate("/");
-    return res.json({ revalidated: true });
+    console.log("clearing cache");
+    fs.readdir(directory, (err, files) => {
+      if (err) throw err;
+      for (const file of files) {
+        fs.unlink(path.join(directory, file), (err) => {
+          if (err) throw err;
+        });
+      }
+    });
+    if (process.env.NODE_ENV == "production") {
+      console.log("revalidating");
+      await res.revalidate("/");
+      return res.json({ revalidated: true });
+    } else {
+      return res.json({ cacheCleared: true });
+    }
   } catch (err) {
     console.log("an error occured", err);
     return res.status(500).send("Error revalidating");
